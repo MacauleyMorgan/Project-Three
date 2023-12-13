c3ec0c2c6427558fc08150f3de45a8d8c176f12e
Macauley Morgan <macauley.main@gmail.com>
2023-12-13 19:51:37 +0000

    Moved login manager to bottom of file

--- cookbook/__init__.py
+++ cookbook/__init__.py
@@ -11,6 +11,13 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 db = SQLAlchemy(app)
 
+from cookbook.models import User
+from cookbook.routes import routes # noqa
+from cookbook.auth import auth # noqa
+
+app.register_blueprint(routes, url_prefix="/")
+app.register_blueprint(auth, url_prefix="/")
+
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
@@ -18,9 +25,3 @@ login_manager.init_app(app)
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
-from cookbook.routes import routes # noqa
-from cookbook.auth import auth # noqa
-
-app.register_blueprint(routes, url_prefix="/")
-app.register_blueprint(auth, url_prefix="/")
