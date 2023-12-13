036ecca6cf776b4c73c234e1619d6aee815f24c2
Macauley Morgan <macauley.main@gmail.com>
2023-12-13 16:26:32 +0000

    Fixed input type on last name in register page Added functionality to print post requests in auth routes

--- cookbook/auth.py
+++ cookbook/auth.py
@@ -1,10 +1,18 @@
 from cookbook import app
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        """
+        This will action when button is clicked to login
+        """
+        email = request.form.get('email')
+        password = request.form.get('password')
+        submission = [email, password]
+        print(submission)
     return render_template("login.html")
 
 
@@ -15,4 +23,16 @@ def logout():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        """
+        This will action when button is clicked to register
+        """
+        first_name = request.form.get('fname')
+        last_name = request.form.get('lname')
+        mobile = request.form.get('mobile')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        user = [first_name, last_name, mobile, email, password1, password2]
+        print(user)
     return render_template("signup.html")
