b2dad8d457722ee0055f8dc8a319c87fc47fbcec
Macauley Morgan <macauley.main@gmail.com>
2023-12-15 19:59:43 +0000

    Allowed recipe form to post to backend

--- cookbook/routes.py
+++ cookbook/routes.py
@@ -21,5 +21,17 @@ def account():
         recipe_ingredients = request.form.get('recipe-ingredients')
         recipe_steps = request.form.get('recipe-steps')
         recipe = [recipe_name, recipe_time, recipe_image, recipe_ingredients, recipe_steps]
-        print(recipe)
+        if len(recipe_name) < 1:
+            flash('Recipe Name is too short!', category='error')
+        elif len(recipe_image) < 1:
+            flash('Recipe image link not provided', category='error')
+        elif len(recipe_ingredients) < 1:
+            flash('Recipe ingredients not provided', category='error')
+        elif len(recipe_steps) < 1:
+            flash('Recipe steps not provided', category='error')
+        else: 
+            new_recipe = Recipes(recipe, user_id=current_user.id)
+            db.session.add(new_recipe)
+            db.session.commit()
+            flash('Recipe uploaded succesfully', category='success')
     return render_template("account.html")
